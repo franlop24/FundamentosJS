46f6c58668a82667ceb6e6f5047fea7950eacfc5
Franlop24 <franlopbri@gmail.com>
2024-09-12 13:44:47 -0600

    02 Operadores

---
+++ concepts/operators.js
@@ -0,0 +1,35 @@
+// Operadores Matemáticos
+// + y -
+// * y /
+// % y ** Módulo y potencia
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 / 2);
+console.log(2 % 2);
+console.log(2 ** 2);
+
+// Operadores de asignación
+let a = 10;
+a += 5; //a = a + 5
+a -= 5;
+a *= 5;
+a /= 5;
+a %= 5;
+a **= 5;
+console.log(a);
+
+// Operadores de Comparación
+console.log(2 > 2);
+console.log(2 < 2);
+console.log(2 >= 2);
+console.log(2 <= 2);
+console.log(2 === 2);
+console.log(2 == 2);
+console.log(2 != 2);
+console.log(2 !== 2);
+
+// Operadores Lógicos
+console.log(true && true); // and
+console.log(true || false); // or
+console.log(!true); // not
