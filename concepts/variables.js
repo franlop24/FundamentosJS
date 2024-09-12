ce5273fec6cecd21badc8bb9fed2ab15c0e59464
Franlop24 <franlopbri@gmail.com>
2024-09-12 13:56:25 -0600

    03 Variables let y const

---
+++ concepts/variables.js
@@ -0,0 +1,18 @@
+// Declaración de variables con let y const
+// var:  en desuso
+
+// variables let: cambiarán de valor
+let age; // declaración
+console.log(age);
+age = 23; // inicialización
+console.log(age);
+
+let name = "Francisco"; // declaración e inicialización
+console.log(name);
+name = "Paco";
+console.log(name);
+
+// constantes const: no cambiarán de valor
+const PI = 3.1416;
+// PI = 3.15;
+console.log(PI);
